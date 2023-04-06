54ecba7b0c1d04e586df48af4cd1b76cf4a09494
Simonaytes <yandexman228@gmail.com>
2023-04-06 15:45:02 +0300

    Put suffle masks to constants

--- Src/Libs/AlphaBlending/AlphaBlending.cpp
+++ Src/Libs/AlphaBlending/AlphaBlending.cpp
@@ -17,6 +17,57 @@ const size_t kMaxFpsStrLen = 20;
 const char   kWindowHeader[]     = "Alpha blending";
 const size_t kTimeCalcAlphaBlend = 1;
 
+const __m512i k0To1And2To3 = _mm512_set_epi8(0x80, 63, 0x80, 61,
+                                             0x80, 59, 0x80, 57,
+                                             0x80, 55, 0x80, 53,
+                                             0x80, 51, 0x80, 49,
+                                             0x80, 47, 0x80, 45,
+                                             0x80, 43, 0x80, 41,
+                                             0x80, 39, 0x80, 37,
+                                             0x80, 35, 0x80, 33,
+                                             0x80, 31, 0x80, 29,
+                                             0x80, 27, 0x80, 25,
+                                             0x80, 23, 0x80, 21,
+                                             0x80, 19, 0x80, 17,
+                                             0x80, 15, 0x80, 13,
+                                             0x80, 11, 0x80,  9,
+                                             0x80,  7, 0x80,  5,
+                                             0x80,  3, 0x80,  1);
+
+const __m512i k0To1And0To3 = _mm512_set_epi8(0x80, 63, 0x80, 63,
+                                             0x80, 59, 0x80, 59,
+                                             0x80, 55, 0x80, 55,
+                                             0x80, 51, 0x80, 51,
+                                             0x80, 47, 0x80, 47,
+                                             0x80, 43, 0x80, 43,
+                                             0x80, 39, 0x80, 39,
+                                             0x80, 35, 0x80, 35,
+                                             0x80, 31, 0x80, 31,
+                                             0x80, 27, 0x80, 27,
+                                             0x80, 23, 0x80, 23,
+                                             0x80, 19, 0x80, 19,
+                                             0x80, 15, 0x80, 15,
+                                             0x80, 11, 0x80, 11,
+                                             0x80,  7, 0x80,  7,
+                                             0x80,  3, 0x80,  3);
+
+const __m512i kZeroEverySecond = _mm512_set_epi8(0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00,
+                                                  0xFF, 0x00, 0xFF, 0x00);
+
 static void UpdateFpsViewer(sf::Text *fps_counter, float fps);
 static void Printfm512i(__m512i a);
 
@@ -214,58 +265,25 @@ void AlphaBlendingAVX512(Image_t* result, const Image_t* backgr, const Image_t* 
         //fg_rb = [00 r0 00 b0][00 r1 00 b1][00 r2 00 b2]...
         //----------------------------------------------------
 
-        unsigned long long mask1 = 0b0101010101010101010101010101010101010101010101010101010101010101;
-        __m512i fg_rb = _mm512_maskz_mov_epi8(mask1, fg);
-        __m512i bg_rb = _mm512_maskz_mov_epi8(mask1, bg);
+        unsigned long long mask = 0b0101010101010101010101010101010101010101010101010101010101010101;
+        __m512i fg_rb = _mm512_maskz_mov_epi8(mask, fg);
+        __m512i bg_rb = _mm512_maskz_mov_epi8(mask, bg);
 
         //----------------------------------------------------
         //fg    = [a0 r0 g0 b0][a1 r1 g1 b1][a2 r2 g2 b2]...
         //fg_ag = [00 a0 00 g0][00 a1 00 g1][00 a2 00 g2]...
         //----------------------------------------------------
 
-        __m512i shuffle_mask1 = _mm512_set_epi8(0x80, 63, 0x80, 61,
-                                                0x80, 59, 0x80, 57,
-                                                0x80, 55, 0x80, 53,
-                                                0x80, 51, 0x80, 49,
-                                                0x80, 47, 0x80, 45,
-                                                0x80, 43, 0x80, 41,
-                                                0x80, 39, 0x80, 37,
-                                                0x80, 35, 0x80, 33,
-                                                0x80, 31, 0x80, 29,
-                                                0x80, 27, 0x80, 25,
-                                                0x80, 23, 0x80, 21,
-                                                0x80, 19, 0x80, 17,
-                                                0x80, 15, 0x80, 13,
-                                                0x80, 11, 0x80,  9,
-                                                0x80,  7, 0x80,  5,
-                                                0x80,  3, 0x80,  1);
-
-        __m512i fg_ag = _mm512_shuffle_epi8(fg, shuffle_mask1);
-        __m512i bg_ag = _mm512_shuffle_epi8(bg, shuffle_mask1);
+        __m512i fg_ag = _mm512_shuffle_epi8(fg, k0To1And2To3);
+        __m512i bg_ag = _mm512_shuffle_epi8(bg, k0To1And2To3);
 
         //----------------------------------------------------
         //fg   = [a0 r0 g0 b0][a1 r1 g1 b1][a2 r2 g2 b2]...
         //fg_a = [00 a0 00 a0][00 a1 00 a1][00 a2 00 a2]...
         //----------------------------------------------------
 
-        __m512i shuffle_mask2 = _mm512_set_epi8(0x80, 63, 0x80, 63,
-                                                0x80, 59, 0x80, 59,
-                                                0x80, 55, 0x80, 55,
-                                                0x80, 51, 0x80, 51,
-                                                0x80, 47, 0x80, 47,
-                                                0x80, 43, 0x80, 43,
-                                                0x80, 39, 0x80, 39,
-                                                0x80, 35, 0x80, 35,
-                                                0x80, 31, 0x80, 31,
-                                                0x80, 27, 0x80, 27,
-                                                0x80, 23, 0x80, 23,
-                                                0x80, 19, 0x80, 19,
-                                                0x80, 15, 0x80, 15,
-                                                0x80, 11, 0x80, 11,
-                                                0x80,  7, 0x80,  7,
-                                                0x80,  3, 0x80,  3);
 
-        __m512i fg_a = _mm512_shuffle_epi8(fg, shuffle_mask2);
+        __m512i fg_a = _mm512_shuffle_epi8(fg, k0To1And0To3);
 
         //----------------------------------------------------
         //fg_ag  = [00 a0 00 g0][00 a1 00 g1][00 a2 00 g2]...
@@ -285,55 +303,20 @@ void AlphaBlendingAVX512(Image_t* result, const Image_t* backgr, const Image_t* 
         //res    = [00 R0 00 B0][00 R1 00 B1][00 R2 00 B2]
         //-----------------------------------------------------
 
-        __m512i res = _mm512_shuffle_epi8(res_rb, shuffle_mask1);
-        #ifdef DEBUG
-            printf("res    = ");
-            Printfm512i(res);
-            printf("\n");
-        #endif
+        __m512i res = _mm512_shuffle_epi8(res_rb, k0To1And2To3);
 
         //-----------------------------------------------------
         //res_ag = [A0 ** G0 **][A1 ** G1 **][A2 ** G2 **]...
         //res    = [A0 R0 G0 B0][A1 R1 G1 B1][A2 R2 G2 B2]
         //-----------------------------------------------------
 
-        __m512i mask = _mm512_set_epi8(0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00,
-                                       0xFF, 0x00, 0xFF, 0x00);
-
-        res = _mm512_or_epi32(res, _mm512_and_epi32(res_ag, mask));
+        res = _mm512_or_epi32(res, _mm512_and_epi32(res_ag, kZeroEverySecond));
 
         _mm512_stream_si512((__m512i*)&process_part[pixel], res);     //put res in memory
-        #ifdef DEBUG
-            printf("res    = ");
-            Printfm512i(res);
-            printf("\n");
-
-            int k = 0;
-            scanf("%c", &k);
-        #endif
     }    
 
     for (size_t y = 0; y < foregr->info.h; y++)          //put processing part to result
     {
-        //printf("[%08x]", process_part[0]);
-
-        //int kfd = 0;
-        //scanf("%c", &kfd);
-
         memcpy(&result->pixels[y*backgr->info.w], process_part + y*foregr->info.w, line_len);
     }
 
