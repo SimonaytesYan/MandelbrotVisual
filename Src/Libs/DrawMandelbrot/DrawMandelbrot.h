382914a33007bbe2504dabb70e877e462a80a97c
Simonaytes <yandexman228@gmail.com>
2024-05-29 00:39:49 +0300

    Change borders

--- Src/Libs/DrawMandelbrot/DrawMandelbrot.h
+++ Src/Libs/DrawMandelbrot/DrawMandelbrot.h
@@ -13,9 +13,9 @@ struct Rectangle
 
 struct MandelbrotParams
 {
-    Rectangle set_border   = {-2, -1.5, 1, 1.5}; //! borders of set in coordinate plane
-    size_t    image_width  = 800;                //! width in pixel of image in which mandelbrot set need to draw
-    size_t    image_height = 800;                //! height in pixel of image in which mandelbrot set need to draw
+    Rectangle set_border   = {-2.5, -1.25, 1.5, 1.35}; //! borders of set in coordinate plane
+    size_t    image_width  = 1920;                //! width in pixel of image in which mandelbrot set need to draw
+    size_t    image_height = 1080;                //! height in pixel of image in which mandelbrot set need to draw
     size_t    radius_2     = 100;                //! square of radius for mandelbrot set calculation
     size_t    iterations   = 256;                //! number of iterations after which point considered stable    
     double    zoom_lvl     = 1;                  //! this parameter uses in moving 
